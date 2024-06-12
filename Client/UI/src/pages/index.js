f83b0c708d2acba7dd97925652448ac6fb178be4
KhanhVan2212 <vannkph22621@fpt.edu.vn>
2024-06-12 21:43:05 +0700

    update category

--- Client/UI/src/pages/index.js
+++ Client/UI/src/pages/index.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import Category from "./CategoryPage/CategoryPage";
 
-export { Category };
-=======
 import Cart from "./CartPage/CartPage";
-
-export { Cart };
->>>>>>> 581cd225a869e115366b6d54fbd7753489cb7fff
+import Category from "./CategoryPage/CategoryPage";
+export { Cart,Category };
