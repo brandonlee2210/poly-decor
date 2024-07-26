4bf26494cc6cf4814cdff7d62ebf533f6809ca67
nhatnvph <nhatngo20102002@gmail.com>
2024-07-26 09:35:18 +0700

    update category page, fix ui notfound page

---
+++ Front-End/src/pages/Category.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import bannerCategoryPage from "../assets/images/banner-category-page.jpg";
+import categoryImage1 from "../assets/images/category-1.1.jpg";
+import ProductItem from "../components/common/ProductItem";
+import Feedback from "../components/common/Feedback";
+import ListStaff from "../components/common/ListStaff";
+import { Pagination } from "antd";
+
+const Category = () => {
+  const productsCate = [
+    {
+      id: 1,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 2,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 3,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 4,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 5,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 6,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 7,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 8,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 9,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+    {
+      id: 10,
+      image: categoryImage1,
+      name: "Bộ Sofa Tân Cổ Điển SF71-123",
+      price: "173.000.000 ₫",
+      initPrice: "193.000.000 ₫",
+    },
+  ];
+  const [current, setCurrent] = useState(3);
+  const onChange = (page) => {
+    console.log(page);
+    setCurrent(page);
+  };
+  return (
+    <>
+      <div className="banner">
+        <img src={bannerCategoryPage} alt="banner" />
+      </div>
+      <div className="container">
+        <div className="flex items-center justify-between mt-10">
+          <h3 className="text-2xl font-bold text-brown-strong">Sắp xếp theo</h3>
+          <div>
+            <span className="px-3 py-1 border border-[#ccc] rounded-lg text-brown-strong font-semibold cursor-pointer duration-200 hover:text-white hover:bg-brown-light active:bg-brown-light mr-3">
+              Giá tăng dần
+            </span>
+            <span className="px-3 py-1 border border-[#ccc] rounded-lg text-brown-strong font-semibold cursor-pointer duration-200 hover:text-white hover:bg-brown-light active:bg-brown-light">
+              Giá giảm dần
+            </span>
+          </div>
+        </div>
+        <div className="mt-10 grid grid-cols-2 gap-5">
+          {productsCate.map((product, index) => (
+            <ProductItem key={index} product={product} />
+          ))}
+        </div>
+        <div className="flex items-center justify-center mt-10">
+          <Pagination current={current} onChange={onChange} total={50} />
+        </div>
+        <Feedback />
+        <ListStaff />
+      </div>
+    </>
+  );
+};
+
+export default Category;
