d842f32ad015640fae88f0d00f2e6f248e6d62c7
Mountain-PNS <sonp89999@gmail.com>
2024-08-03 21:00:53 +0700

    update auth

--- Back-End/src/controllers/AuthController.js
+++ Back-End/src/controllers/AuthController.js
@@ -1,6 +1,5 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
-
 // handle errors
 const handleErrors = (err) => {
   console.log(err.message, err.code);
@@ -8,37 +7,32 @@ const handleErrors = (err) => {
 
   // duplicate username error
   if (err.code === 11000) {
-    errors.username = "Tên người dùng này đã được đăng ký";
+    errors.username = "that username is already registered";
     return errors;
   }
 
   // validation errors
   if (err.message.includes("user validation failed")) {
+    // console.log(err);
     Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
       errors[properties.path] = properties.message;
     });
     return errors;
   }
 
-  // incorrect username
+  // incorrectusername
   if (err.message === "incorrect username") {
-    errors.username = "Tên người dùng không tồn tại";
+    errors.username = "That username is not registered";
     return errors;
   }
 
   // incorrect password
   if (err.message === "incorrect password") {
-    errors.password = "Mật khẩu không chính xác";
+    errors.password = "Password is not correct";
     return errors;
   }
-
-  // general errors
-  if (err.message) {
-    errors.general = err.message;
-    return errors;
-  }
-
-  return errors;
 };
 
 const maxAge = 3 * 60 * 60 * 24;
@@ -58,19 +52,8 @@ export const login_get = (req, res) => {
 };
 
 export const signup_post = async (req, res) => {
-  const { username, password } = req.body;
-
-  // validate inputs
-  if (!username || !password) {
-    return res.status(400).json({ errors: { general: "Tên người dùng và mật khẩu là bắt buộc" } });
-  }
-
-  if (password.length < 6) {
-    return res.status(400).json({ errors: { password: "Mật khẩu phải có ít nhất 6 ký tự" } });
-  }
-
   try {
-    const user = await User.create({ username, password });
+    const user = await User.create(req.body);
     const token = createToken(user._id);
     res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({ user: user._id });
@@ -83,14 +66,10 @@ export const signup_post = async (req, res) => {
 export const login_post = async (req, res) => {
   const { username, password } = req.body;
 
-  // validate inputs
-  if (!username || !password) {
-    return res.status(400).json({ errors: { general: "Tên người dùng và mật khẩu là bắt buộc" } });
-  }
-
   try {
     const user = await User.login(username, password);
     const token = createToken(user._id);
+
     res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(200).json({ user: user._id });
   } catch (err) {
