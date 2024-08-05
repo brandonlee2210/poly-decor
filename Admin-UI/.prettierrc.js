a60a2382cef455e58a811243e99650237e0ef2f9
brandonlee2110 <baodgph30207@fpt.edu.vn>
2024-08-05 21:35:19 +0700

    feat: init dự án

---
+++ Admin-UI/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  // config  end  of  line  auto cr and crlf
+  endOfLine: 'auto', //  auto:  自动根据系统的换行符进行换行,\
+};
